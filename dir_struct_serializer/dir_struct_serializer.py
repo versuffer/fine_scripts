a95d88b9c5970af5a5c0c634f3d95f049c677e7c
Sergey Nosov <93401612+versuffer@users.noreply.github.com>
2023-02-09 09:16:38 +0300

    Refactoring

--- dir_struct_serializer/dir_struct_serializer.py
+++ dir_struct_serializer/dir_struct_serializer.py
@@ -14,10 +14,11 @@ def tree_to_dict(initial_dir_path, tree_dict=None):
     dir_content = pathlib.Path(initial_dir_path).iterdir()
 
     for file_path in dir_content:
-        try:
-            branch_name = file_path.name
-            branch_content = {'type': 'file'}
 
+        branch_name = file_path.name
+        branch_content = {'type': 'file'}
+
+        try:
             if file_path.is_dir():
                 branch_content = tree_to_dict(file_path)
                 tree_dict[dir_name]['children'][branch_name] = branch_content[branch_name]
